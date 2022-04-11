34d765cfd28dc3f478e2a90dd274c718b134bae6
Ansh Tripathi <46096778+anshtripathi01@users.noreply.github.com>
2022-04-11 13:51:07 -0700

    HomePage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./Components/Header";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        </Routes>
     </div>
   );
 }
